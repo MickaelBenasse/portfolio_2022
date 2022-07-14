1e0d8d0e634eb4324c2bfd56e90adecec5ecb9c5
mickael <mickael@soloso.fr>
2022-07-14 14:53:36 +0300

    Creation of the AnimatedBlob with his story

---
+++ src/stories/animatedBlob.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import AnimatedBlob from "../component/animatedBlob/AnimatedBlob";
+
+export default {
+  title: "Animated blob",
+  component: AnimatedBlob,
+};
+
+export const Blob = () => <AnimatedBlob />;
