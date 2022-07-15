69b82495c9636a9ceaef665f59a611e8a5c2e3ca
mickael <mickael@soloso.fr>
2022-07-15 13:11:40 +0300

    Remove unused import.

--- src/component/animatedBlob/AnimatedBlob.jsx
+++ src/component/animatedBlob/AnimatedBlob.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useLayoutEffect } from "react";
 import { random } from "../../functions/functions";
 
 export default function AnimatedBlob({ title, colorPalette, img, underText }) {
