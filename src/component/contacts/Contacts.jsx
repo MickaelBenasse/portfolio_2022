bd3f9332cf1de2eff58147096a4579c8b177f3c0
mickael <mickael@soloso.fr>
2022-07-16 13:36:36 +0300

    Test

--- src/component/contacts/Contacts.jsx
+++ src/component/contacts/Contacts.jsx
@@ -41,6 +41,48 @@ export default function Contacts() {
       contact5.style.transform = `translateX(${(-scrolling + 2900) * 0.4}px)`;
       contact6.style.transform = `translateX(${scrolling - 5000}px)`;
     });
+
+    const cursor = document.getElementById("cursor");
+    const cursorPoint = document.getElementById("cursor-point");
+    const cursorEmail = document.querySelectorAll(".cursor-email");
+
+    // For all the elements with the class "cursor-email" with apply those modifications.
+    // Cursor effect with the "Email me" inside.
+    cursorEmail.forEach((thisCursorHover) => {
+      // Change the cursor to "Email me"
+      thisCursorHover.addEventListener("mouseenter", () => {
+        cursor.style.height = "100px";
+        cursor.style.width = "100px";
+        cursor.style.opacity = "1";
+        cursor.style.backgroundColor = "#606C38";
+        cursorPoint.style.opacity = "0";
+
+        if (cursor.children.length === 0) {
+          const element = document.createElement("p");
+
+          const textNode = document.createTextNode("Email me");
+          cursor.style.cssText =
+            "font-family: 'Roboto', sans-serif; font-weight: 400; font-size: 1.25rem; display: flex; justify-content: center; align-items: center; text-align: center;";
+          element.appendChild(textNode);
+
+          cursor.appendChild(element);
+        }
+
+        console.log(cursor);
+      });
+
+      // Remove the text and put the cursor back to normal.
+      thisCursorHover.addEventListener("mouseleave", () => {
+        cursorPoint.style.opacity = "1";
+
+        cursor.style.height = "30px";
+        cursor.style.width = "30px";
+        cursor.style.backgroundColor = "transparent";
+        if (cursor.children.length === 1) {
+          cursor.firstChild.remove();
+        }
+      });
+    });
   });
 
   function handleEmailMe() {
--- src/component/navigationBar/NavigationBar.jsx
+++ src/component/navigationBar/NavigationBar.jsx
@@ -19,8 +19,6 @@ export default function NavigationBar() {
   useLayoutEffect(() => {
     const cursorHover = document.querySelectorAll(".cursor-hover");
     const cursor = document.getElementById("cursor");
-    const cursorPoint = document.getElementById("cursor-point");
-    const cursorEmail = document.querySelectorAll(".cursor-email");
 
     // Classic hover with opacity
     cursorHover.forEach(function (elementHover) {
@@ -35,44 +33,6 @@ export default function NavigationBar() {
         cursor.style.height = "30px";
         cursor.style.width = "30px";
         cursor.style.backgroundColor = "transparent";
-      });
-    });
-
-    // For all the elements with the class "cursor-email" with apply those modifications.
-    // Cursor effect with the "Email me" inside.
-    cursorEmail.forEach((thisCursorHover) => {
-      // Change the cursor to "Email me"
-      thisCursorHover.addEventListener("mouseenter", () => {
-        cursor.style.height = "100px";
-        cursor.style.width = "100px";
-        cursor.style.opacity = "1";
-        cursor.style.backgroundColor = "#606C38";
-        cursorPoint.style.opacity = "0";
-
-        if (cursor.children.length === 0) {
-          const element = document.createElement("p");
-
-          const textNode = document.createTextNode("Email me");
-          cursor.style.cssText =
-            "font-family: 'Roboto', sans-serif; font-weight: 400; font-size: 1.25rem; display: flex; justify-content: center; align-items: center; text-align: center;";
-          element.appendChild(textNode);
-
-          cursor.appendChild(element);
-        }
-
-        console.log(cursor);
-      });
-
-      // Remove the text and put the cursor back to normal.
-      thisCursorHover.addEventListener("mouseleave", () => {
-        cursorPoint.style.opacity = "1";
-
-        cursor.style.height = "30px";
-        cursor.style.width = "30px";
-        cursor.style.backgroundColor = "transparent";
-        if (cursor.children.length === 1) {
-          cursor.firstChild.remove();
-        }
       });
     });
   });
