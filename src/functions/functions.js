3631c588bb0a080b3dff41f69aa6f7fd604ef87a
mickael <mickael@soloso.fr>
2022-07-16 11:32:40 +0300

    Put scrollTo and copy function to the file function to be able to get them through the entire app.

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -7,3 +7,18 @@ export function random(min, max) {
 export function map(n, start1, end1, start2, end2) {
   return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
 }
+
+// Animation to the anchor
+export function scrollTo(element) {
+  const thisElement = document.getElementById(element);
+  thisElement.scrollIntoView({
+    block: "start",
+    behavior: "smooth",
+    inline: "start",
+  });
+}
+
+// Copy to clipboard the email
+export function copy() {
+  navigator.clipboard.writeText("mickaelbenasse@gmail.com").then();
+}
